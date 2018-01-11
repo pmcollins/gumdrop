2638249ccefd621c310c78b2a883dd36e0379d60
pmcollins <pablo.collins@gmail.com>
2018-01-11 10:37:32 -0500

    Interactors

--- gumdrop.web/gumdrop/web/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package gumdrop.web;
-
-public class ValidationException extends Exception {
-
-  ValidationException(String key, String value) {
-    super("key [" + key + "], value: [" + value + "]");
-  }
-
-}
