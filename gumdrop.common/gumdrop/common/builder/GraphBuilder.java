51f63a0b80b36eb2f0e1ab64064a1305e9601048
pmcollins <pablo.collins@gmail.com>
2018-03-23 10:19:45 -0400

    README

---
+++ gumdrop.common/gumdrop/common/builder/GraphBuilder.java
@@ -0,0 +1,29 @@
+package gumdrop.common.builder;
+
+/**
+ * Binds a Builder object to an instance whose type corresponds to the Builder
+ */
+public class GraphBuilder<T> {
+
+  private final Builder<T> builder;
+  private final T t;
+
+  public GraphBuilder(Builder<T> builder) {
+    this.builder = builder;
+    t = builder.construct();
+  }
+
+  public GraphBuilder<?> constructAndSet(String key) {
+    SetterBinding<T, ?> setterBinding = builder.getMember(key);
+    return setterBinding.constructAndSet(t, key);
+  }
+
+  public void applyString(String key, String value) {
+    builder.apply(t, key, value);
+  }
+
+  public T getObject() {
+    return t;
+  }
+
+}
