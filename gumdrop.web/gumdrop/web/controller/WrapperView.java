ee5880beaceb3343514d80c4e13beb1bb62820a1
pmcollins <pablo.collins@gmail.com>
2019-02-22 17:23:23 -0500

    passing classname into layout wrapper

--- gumdrop.web/gumdrop/web/controller/WrapperView.java
+++ gumdrop.web/gumdrop/web/controller/WrapperView.java
@@ -4,6 +4,6 @@ import gumdrop.web.html.Buildable;
 
 public interface WrapperView<M> {
 
-  void wrap(StringBuilder sb, M m, Buildable buildable);
+  void wrap(StringBuilder sb, M m, Buildable buildable, String className);
 
 }
