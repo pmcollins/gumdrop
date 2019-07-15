df78a3e810959bd9d5c71355ace9e9526511e90e
pmcollins <pablo.collins@gmail.com>
2019-07-14 23:15:44 -0400

    list node extend collection node

--- gumdrop.json/gumdrop/json/v2/ListNode.java
+++ gumdrop.json/gumdrop/json/v2/ListNode.java
@@ -6,19 +6,10 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class ListNode<T> extends NullableNode<List<T>> {
-
-  private final Function<Consumer<T>, Node<T>> nodeConstructor;
+public class ListNode<T> extends CollectionNode<List<T>, T> {
 
   public ListNode(Function<Consumer<T>, Node<T>> nodeConstructor) {
-    this.nodeConstructor = nodeConstructor;
-  }
-
-  @Override
-  public Chainable next() {
-    List<T> list = new ArrayList<>();
-    setValue(list);
-    return new ListElementNode<>(list, nodeConstructor);
+    super(ArrayList::new, List::add, nodeConstructor);
   }
 
 }
