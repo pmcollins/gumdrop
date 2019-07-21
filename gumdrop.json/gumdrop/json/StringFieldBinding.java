a33d7e6d2adc703f71ea8ee8dcc346b841ce8957
pmcollins <pablo.collins@gmail.com>
2019-07-20 20:42:52 -0400

    stringfield binding enum constructor

--- gumdrop.json/gumdrop/json/StringFieldBinding.java
+++ gumdrop.json/gumdrop/json/StringFieldBinding.java
@@ -4,6 +4,10 @@ import java.util.function.BiConsumer;
 
 public class StringFieldBinding<T> extends FieldBinding<T, String> {
 
+  public StringFieldBinding(Enum<?> e, BiConsumer<T, String> setter) {
+    this(e.name().toLowerCase(), setter);
+  }
+
   public StringFieldBinding(String name, BiConsumer<T, String> setter) {
     super(name, setter, StringNode::new);
   }
